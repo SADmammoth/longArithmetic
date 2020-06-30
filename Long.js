be887fda3e8a0151c7388ba2ac68331047d10ab4
SADmammoth <lo.ma200018@gmail.com>
2020-06-30 20:43:24 +0300

    feat: slice and mutating slice

--- Long.js
+++ Long.js
@@ -3,7 +3,6 @@ function Long(numberString) {
 
   let number;
   if (typeof numberString === 'number') {
-    console.log(numberString);
     number = [parseInt(numberString.toString(10).slice(0, chunk))];
   } else {
     number = numberString
@@ -19,9 +18,48 @@ function Long(numberString) {
     toString: () => {
       return self.number.join('');
     },
+    slice: (limit) => {
+      return {
+        number: sliceNumber(self.number, limit),
+        ...methods,
+      };
+    },
+    mutatingSlice: (limit) => {
+      self.number = sliceNumber(self.number, limit);
+    },
   };
 
   Object.entries(methods).forEach(([name, method]) => (self[name] = method));
 
   return self;
+}
+
+function sliceInt(int, limit, chunk) {
+  return int / 10 ** (chunk - limit);
+}
+
+function sliceNumber(number, limit, chunk) {
+  if (limit >= chunk * number.length) {
+    return number;
+  }
+
+  let int = parseInt(limit / chunk);
+  let mod = limit % chunk;
+
+  if (limit < chunk) {
+    return [parseInt(sliceInt(number[0], limit))];
+  }
+
+  if (!mod) {
+    return sliceByChunk();
+  }
+
+  return [
+    ...number.slice(0, int),
+    parseInt(sliceInt(sliceByChunk()[int], mod)),
+  ];
+}
+
+function sliceByChunk() {
+  return number.slice(0, int + 1);
 }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (function () {
   let long = new Long(123456781);
+  long.mutatingSlice(17);
   console.log(long);
 })();
