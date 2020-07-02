b639a06379ab4b100dee5b1ce0c8470d12ae10b8
SADmammoth <Lo.ma200018@gmail.com>
2020-07-02 15:46:11 +0300

    feat: sum of numbers

--- Long.js
+++ Long.js
@@ -1,17 +1,21 @@
-function Long(numberString) {
+function Long(numberString, degree) {
   let chunk = 8;
-
   let number;
+  let numberLength = degree;
   if (typeof numberString === 'number') {
     number = [parseInt(numberString.toString(10).slice(0, chunk))];
+    if (!degree) numberLength = numberString.toString(10).length;
   } else {
+    if (!degree) numberLength = numberString.length;
     number = numberString
+      //.replace(/((?:^.*[^0])|(?:^))[0]+$/,'$10')
       .match(new RegExp(`[0-9]{1,${chunk}}`, 'g'))
       .map((el) => parseInt(el));
   }
 
   let self = {
     number,
+    degree: numberLength,
   };
 
   let methods = {
@@ -19,19 +23,74 @@ function Long(numberString) {
       return self.number.join('');
     },
     slice: (limit) => {
-      return {
-        number: sliceNumber(self.number, limit),
-        ...methods,
-      };
+      return new Long(sliceNumber(self.number, limit).join(''));
     },
     mutatingSlice: (limit) => {
       self.number = sliceNumber(self.number, limit);
+    },
+    add: (long) => {
+      if (self.number[0].length <= chunk && long.number[0].length <= chunk) {
+        return new Long(
+          self.number[0] * 10 ** self.degree + long.number[0] * 10 ** sf.degree
+        );
+      }
+      let overflow = 0;
+      let newNumber = [];
+
+      function sum(left, right, overflow) {
+        return left + right + overflow;
+      }
+
+      let prevSum =
+        self.number[self.number.length - 1] +
+        long.number[long.number.length - 1];
+      let numLength = (num) => num.toString(10).length;
+      let newDegree;
+      if (long.degree <= self.degree) {
+        newDegree = self.degree;
+      } else {
+        newDegree = long.degree;
+      }
+
+      if (
+        self.number[self.number.length - 1] === 0 &&
+        long.number[long.number.length - 1] !== 0
+      ) {
+        let power = self.degree - chunk * (self.number.length - 1);
+        console.log(power, self.degree);
+        prevSum =
+          10 ** power + long.number[long.number.length - 1] - 10 ** power;
+        console.log();
+        overflow = parseInt(prevSum / 10 ** power);
+      } else {
+        overflow = parseInt(prevSum / 10 ** chunk);
+      }
+      newNumber.push(getLast(prevSum, chunk));
+      console.log(overflow, newNumber, long.number[long.number.length - 1]);
+      for (let i = self.number.length - 2; i >= 0; i--) {
+        prevSum = self.number[i] + long.number[i] + overflow;
+        overflow = parseInt(prevSum / 10 ** chunk);
+        newNumber.push(getLast(prevSum, chunk));
+      }
+
+      console.log(overflow, newNumber);
+
+      if (overflow !== 0) {
+        newNumber.push(overflow);
+        newDegree += overflow.toString(10).length;
+      }
+
+      return new Long(newNumber.reverse().join(''), newDegree);
     },
   };
 
   Object.entries(methods).forEach(([name, method]) => (self[name] = method));
 
   return self;
+}
+
+function getLast(int, count) {
+  return int % 10 ** count;
 }
 
 function sliceInt(int, limit, chunk) {
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function () {
-  let long = new Long(123456781);
-  long.mutatingSlice(17);
-  console.log(long);
+  let long = new Long('1000000000');
+  let long2 = new Long('1000000');
+  console.log(long.add(long2));
 })();
